7c12f453967e55ff51992284f034c8611d877bff
somnatha656 <somnatha656@gmail.com>
2022-09-15 21:41:34 +0100

    my first commit

--- src/githubpractice3a/Practice3a.java
+++ src/githubpractice3a/Practice3a.java
@@ -3,7 +3,7 @@ package githubpractice3a;
 public class Practice3a {
     public static void main(String[] args) {
         int a = 10;
-        int b = 15;
+        int b = 20;
         int c = a*b;
 
         System.out.println(a);
@@ -12,5 +12,6 @@ public class Practice3a {
         System.out.println(a+b);
         System.out.println(c+a);
         System.out.println(c+b);
+        System.out.println(b/a);
     }
 }
